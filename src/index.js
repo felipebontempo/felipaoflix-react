cabc692197142fc4de15dcb561bcccb7b1e12f3c
felipebontempo <felipe.bontempo@gmail.com>
2020-07-28 15:06:36 -0300

    Instalacao do React Router e preparando a utilizacao do mesmo
    Já foi feito a importação no arquivo principal index.js, no qual importa
    o 'react-router-rom', e os métodos BrowserRouter, Switch e Route.
    
    No ReactDOM, utilizo o BrowserRouter, Route e Switch para criar a rota
    principal, na tag Route uso a palavra 'exact' para informar que caso o
    usuário tente acessar uma roto diferente e a mesma não existir será
    exibido uma página em branco e não a página principal, o comportamento
    padrão é caso não consiga achar a rota mostra a página principal.
    
    Por último, fiz a alteracao da descricao no vídeo principal.

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={App} exact />
+    </Switch>
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
